0118babea8d577913fe1494378e952fccb8109da
Islam-Jumabekov <islam.rusruslanov@mail.ru>
2021-05-30 21:34:05 +0600

    Auth.nargin.classname.src

--- src/components/Auth/Auth.js
+++ src/components/Auth/Auth.js
@@ -1,62 +1,47 @@
-import React from "react";
-import axios from "../../axios";
-import { start, auth } from "../../store/actions/auth";
-import withAxios from "../withAxios";
 import classes from "./Auth.module.css";
-import Button from "../UI/Button/Button";
+import { useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
-import { Redirect, useLocation } from "react-router-dom";
+import { Redirect } from "react-router";
+import { auth, start } from "../../store/actions/auth";
 
-export default withAxios(() => {
+const Auth = () => {
   const dispatch = useDispatch();
-  const { loading, error, token } = useSelector(state => state.auth);
-  const location = useLocation();
+  const { token, error, loading } = useSelector(state => state.auth);
+  const [method, setMethod] = useState("signin");
 
-  const formSubmitted = (event) => {
-    start(dispatch);
+  const submitCallback = (event) => {
+    event.preventDefault();
+
+    dispatch(start());
 
     const data = new FormData(event.target);
-    const method = event.nativeEvent.submitter.innerText === "Sign in"
-      ? "signin" : "signup";
-    auth(dispatch, method, data.get('email'), data.get('password'));
 
-    event.preventDefault();
-  }
-
-  let formOutput = "Loading...";
-  if (!loading) {
-    formOutput = (
-      <form onSubmit={formSubmitted}>
-        <h1>Welcome</h1>
-        <input type="email" placeholder="E-mail" name="email" required />
-        <input type="password" placeholder="Password" name="password" required minLength="6" />
-      <div className={classes.Buttons}>
-          <Button>Sign in</Button>
-        <Button>Sign up</Button>
-      </div>
-      
-      </form>
-    );
+    dispatch(auth(method, data.get('email'), data.get('password')));
   }
 
   let errorOutput = null;
   if (error) {
-    errorOutput = <h4 className = {classes.error}>{error.message}</h4>;
+    errorOutput = <h4 style={{ color: "red" }}>{error}</h4>;
   }
 
-  const [,redirect] = location.search.split('?');
   let redirectOutput = null;
-  if (token !== null) {
-    redirectOutput = <Redirect to={"/" + redirect ? redirect : ""} />;
+  if (token) {
+    redirectOutput = <Redirect to="/" />
   }
 
   return (
     <div className={classes.Auth}>
+      <h1>Welcome</h1>
       {errorOutput}
-      {formOutput}
+      <form onSubmit={submitCallback}>
+        <input type="email" name="email" placeholder="Email" required />
+        <input type="password" name="password" placeholder="Password" required />
+        <button onClick={() => setMethod("signin")} disabled={loading}>Sign in</button>
+        <button onClick={() => setMethod("signup")} disabled={loading}>Sign up</button>
+      </form>
       {redirectOutput}
     </div>
   );
-}, axios);
-
-
+}
+ 
+export default Auth;
