8a4dfb1d2f52d0c9c0a6e0d5eaaf1e3259037cfc
Islam-Jumabekov <islam.rusruslanov@mail.ru>
2021-05-31 14:17:13 +0600

    axios

--- src/axios.js
+++ src/axios.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
 const instance = axios.create();
-instance.defaults.baseURL = "https://builder-9f6b5-default-rtdb.firebaseio.com";
+instance.defaults.baseURL = "https://builder-c2fd7-default-rtdb.firebaseio.com/";
 
 export default instance;
